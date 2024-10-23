d1dc254c504da8dd92e1c38f11ba590d2c6cfb25
NigelMupira <h230828v@hit.ac.zw>
2024-10-23 22:50:13 +0200

    Update PasswordUtils.java
    This class includes a method to generate strong passwords.

--- src/main/java/com/securelogin/PasswordUtils.java
+++ src/main/java/com/securelogin/PasswordUtils.java
@@ -1,4 +1,16 @@
 package main.java.com.securelogin;
 
+import java.security.SecureRandom;
+
 public class PasswordUtils {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static String generateStrongPassword(int length) {
+        StringBuilder password = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            password.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return password.toString();
+    }
 }
