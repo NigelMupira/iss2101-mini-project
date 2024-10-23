3f41652aa60ce4a6ade22c5ff26259a5b209c6e7
NigelMupira <h230828v@hit.ac.zw>
2024-10-23 22:49:45 +0200

    Update AuthService.java
    This class manages authentication logic.

--- src/main/java/com/securelogin/AuthService.java
+++ src/main/java/com/securelogin/AuthService.java
@@ -1,4 +1,13 @@
 package main.java.com.securelogin;
 
 public class AuthService {
+    private UserService userService;
+
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public User authenticate(String email, String password) {
+        return userService.login(email, password);
+    }
 }
